6875de9a328d61920a1a8fc3e6e817c2a4f50185
Adam Pearson <adam.pearson.1996@gmail.com>
2020-03-15 19:44:41 +1100

    Update GetInTouchHero.js

--- src/components/GetInTouchHero/GetInTouchHero.js
+++ src/components/GetInTouchHero/GetInTouchHero.js
@@ -2,48 +2,44 @@ import React from 'react';
 import './GetInTouchHero.scss';
 
 class GetInTouchHero extends React.Component {
-  state = {
-    name: "",
-    email: ""
-  }
+  // state = {
+  //   name: "",
+  //   email: ""
+  // }
 
-  handleInputChange = event => {
-    const target = event.target
-    const value = target.value
-    const name = target.name
-    this.setState({
-      [name]: value,
-    })
-  }
+  // // handleInputChange = event => {
+  // //   const target = event.target
+  // //   const value = target.value
+  // //   const name = target.name
+  // //   this.setState({
+  // //     [name]: value,
+  // //   })
+  // // }
 
-  handleSubmit = event => {
-    event.preventDefault()
-    alert(`Welcome ${this.state.name}, ${this.state.email}!`)
-  }
+  // // handleSubmit = event => {
+  // //   event.preventDefault()
+  // //   alert(`Welcome ${this.state.name}, ${this.state.email}!`)
+  // // }
 
   render() {
     return (
       <section id="get-in-touch-hero">
         <div className="outer-container">
           <h3>Get in touch</h3>
-          <form onSubmit={this.handleSubmit}>
+          <form name="Get in touch" netlify>
             <label>
               Name
               <input
                 type="text"
                 name="name"
-                placeholder="name"
-                value={this.state.name}
-                onChange={this.handleInputChange} />
+                placeholder="name" />
             </label>
             <label>
               Email
               <input
               type="email" 
               name="email"
-              placeholder="email"
-              value={this.state.email}
-              onChange={this.handleInputChange} />
+              placeholder="email" />
             </label>
             <button type="submit">Submit</button>
           </form>
